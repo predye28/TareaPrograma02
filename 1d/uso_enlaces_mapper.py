6918d00dd7af8228b0557aa617aac35a855cdf2f
Rayforth12 <56418246+Rayforth12@users.noreply.github.com>
2023-11-18 15:29:08 -0600

    d actualizado

--- 1d/uso_enlaces_mapper.py
+++ 1d/uso_enlaces_mapper.py
@@ -7,8 +7,6 @@ for line in sys.stdin:
     row = list(csv.reader([line]))[0]
     if len(row) >= 6 and row[0].lower() != "page_title":
         page_title = row[0]
-        references = row[5].split("|")
-        
-        for reference in references:
-            if reference:
-                print(f"{page_title}\t{reference}\t1")
+        description = row[6]  # Ajusta el índice según la posición correcta de la descripción
+        # Emitir la descripción completa como una sola entrada
+        print(f"{page_title}\t{description}\t1")
--- 1d/uso_enlaces_reducer.py
+++ 1d/uso_enlaces_reducer.py
@@ -3,22 +3,24 @@
 import sys
 
 current_key = None
-reference_counts = {}
+description_counts = {}
 
 for line in sys.stdin:
-    key, reference, count = line.strip().split("\t")
+    key, description, count = line.strip().split("\t")
     count = int(count)
 
     if current_key == key:
-        reference_counts[reference] = reference_counts.get(reference, 0) + count
+        if description:  # Verificar si la descripción no está vacía
+            description_counts[description] = description_counts.get(description, 0) + count
     else:
-        if current_key:
-            for ref, ref_count in reference_counts.items():
-                print(f"{current_key}\t{ref}\t{ref_count}")
+        if current_key is not None:
+            for desc, desc_count in description_counts.items():
+                print(f"{current_key}\t{desc}\t{desc_count}")
         current_key = key
-        reference_counts = {reference: count}
+        description_counts = {description: count if description else 0}
 
 # Imprimir el resultado para la última clave
-if current_key:
-    for ref, ref_count in reference_counts.items():
-        print(f"{current_key}\t{ref}\t{ref_count}")
+if current_key is not None:
+    for desc, desc_count in description_counts.items():
+        if desc:  # Verificar si la descripción no está vacía
+            print(f"{current_key}\t{desc}\t{desc_count}")
